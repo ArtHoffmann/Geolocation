922210be9bc64854062e4667d937d4580257f4ea
unknown <you@example.com>
2019-06-16 22:51:11 +0200

    asd

--- src/app/wegstrecke/wegstrecke.page.ts
+++ src/app/wegstrecke/wegstrecke.page.ts
@@ -1,3 +1,4 @@
+import { FotoPage } from './../foto/foto.page';
 import { ListPage } from './../list/list.page';
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
@@ -13,6 +14,8 @@ import {
 } from '@ionic-native/google-maps';
 import { FotoPage } from '../foto/foto.page';
 import { Observable, timer, Subscription } from 'rxjs';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-wegstrecke',
@@ -20,6 +23,8 @@ import { Observable, timer, Subscription } from 'rxjs';
   styleUrls: ['./wegstrecke.page.scss'],
 })
 export class WegstreckePage implements OnInit {
+
+  anOtherPage: FotoPage;
 
   geoLatitude: number;
   geoLongitude: number;
@@ -32,6 +37,11 @@ export class WegstreckePage implements OnInit {
   geoAccuracy: number;
   geoAddress: string;
 
+  geoencoderOptions: NativeGeocoderOptions = {
+    useLocale: true,
+    maxResults: 5
+  };
+
   watchLocationUpdates: any;
   loading: any;
   isWatching: boolean;
@@ -42,20 +52,114 @@ export class WegstreckePage implements OnInit {
   running = false;
   startText = 'Start';
 
+  public photos: any;
+  public base64Image: string;
+  public fileImage: string;
+  public responseData: any;
+  userData = { user_id: '', token: '', imageB64: '' };
 
-  subscribe: Subscription;
+  takePhoto() {
+    console.log('coming here');
 
-  geoencoderOptions: NativeGeocoderOptions = {
-    useLocale: true,
-    maxResults: 5
-  };
+    const options: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      targetWidth: 450,
+      targetHeight: 450,
+      saveToPhotoAlbum: true
+    };
+
+    this.camera.getPicture(options).then(
+      imageData => {
+        this.base64Image = 'data:image/jpeg;base64,' + imageData;
+        this.photos.push(this.base64Image);
+        this.photos.reverse();
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+
   constructor(
     private geolocation: Geolocation,
     private nativeGeocoder: NativeGeocoder,
-    private storage: Storage
+    private storage: Storage,
+    private camera: Camera,
+    public navCtrl: NavController,
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    // this.loadSaved();
+    this.loadMap();
+  }
+
+
+  loadMap() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      const map = GoogleMaps.create('map');
+      map.one(GoogleMapsEvent.MAP_READY).then((data: any) => {
+
+        const coordinates: LatLng = new LatLng(resp.coords.latitude, resp.coords.longitude);
+        console.log('datensatz ' + resp.coords.longitude, resp.coords.latitude);
+        const position = {
+          target: coordinates,
+          zoom: 14
+        };
+        const coordinatesEnd: LatLng = new LatLng(resp.coords.latitude + 0.1, resp.coords.longitude + 0.1);
+        console.log('datensatz ' + resp.coords.longitude, resp.coords.latitude);
+
+        map.animateCamera(position);
+
+        const markerOptions: MarkerOptions = {
+          position: coordinates,
+          title: 'Hier bin ich'
+        };
+        const markerOptionsEnd: MarkerOptions = {
+          position: coordinatesEnd,
+          title: 'Ziel'
+        };
+
+        const marker = map.addMarker(markerOptions)
+          // tslint:disable-next-line:no-shadowed-variable
+          .then((marker: Marker) => {
+            marker.showInfoWindow();
+          });
+        const markerEnd = map.addMarker(markerOptionsEnd)
+          // tslint:disable-next-line:no-shadowed-variable
+          .then((marker: Marker) => {
+            marker.showInfoWindow();
+          });
+        const points = [];
+        // points.push({
+        //   lat: resp.coords.latitude,
+        //   lng: resp.coords.longitude
+        // });
+        // points.push({
+        //   lat: resp.coords.latitude + 0.01,
+        //   lng: resp.coords.longitude + 0.2
+        // });
+        // points.push({
+        //   lat: resp.coords.latitude + 0.1,
+        //   lng: resp.coords.longitude + 0.23
+        // });
+        // points.push({
+        //   lat: resp.coords.latitude + 0.13,
+        //   lng: resp.coords.longitude - 0.3
+        // });
+
+        map.addPolyline({
+          points,
+          color: '#AA00FF',
+          width: 5,
+          geodesic: true
+        });
+      });
+    });
+  }
 
   startTimer() {
     this.running = !this.running;
@@ -130,6 +234,7 @@ export class WegstreckePage implements OnInit {
       this.geoStopLongitude = resp.coords.longitude;
     });
     this.calculateDistanceInKm();
+    this.takePhoto();
   }
 
   getPosition() {
